54e1d34858fcd555847dd3ea217ae804644ec946
keisuke.iida <ke-iida@kddi-tech.com>
2022-02-13 10:25:47 +0900

    upload SecondKadai

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Keisuke Iida on 2022/02/12.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = "名無し"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        label.text = "こんにちは\(name)さん"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.delegate = self
     }
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range:NSRange, replacementString string: String)->Bool{
+//        return true
+//    }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        //resultViewController.name = "iida"
+        if let buf:String = textField.text {
+        resultViewController.name = buf
+        }
+        //resultViewController.name = textField.text
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
+    
 }
 
